9546daad6db8334562729e10718310d831f883f6
Alexander McCall <146675021+alexmc-c@users.noreply.github.com>
2024-11-27 13:49:22 +0000

    PFAS: Added stylesheet to show labels

--- src/fluorinatedcompounds.cpp
+++ src/fluorinatedcompounds.cpp
@@ -3,15 +3,20 @@
 #include <QHBoxLayout>
 #include <QLabel>
 
+#include "styles.h"
+
 FluorinatedCompounds::FluorinatedCompounds(QWidget* parent)
     : BasePage("Fluorinated Compounds Page", parent) {
   setupUI();
+  setStyleSheet(Styles::combineStyleSheets({":/styles/basepage.qss",
+                                            ":/styles/fluorinatedcompounds.qss"}));
 }
 
 void FluorinatedCompounds::setupUI() {
   QHBoxLayout* pageLayout = new QHBoxLayout();
 
   QLabel* exampleLabel = new QLabel("This is an example page for FluorinatedCompounds!");
+  exampleLabel->setObjectName("h1");
   pageLayout->addWidget(exampleLabel);
 
   contentArea->setLayout(pageLayout);
