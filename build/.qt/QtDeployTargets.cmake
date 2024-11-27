d8a30f9432d3ddcf840d4b4e3e1c08739dccfe90
AlexJGeorge1 <sc23a2g@leeds.ac.uk>
2024-11-27 12:47:19 +0000

    Completed initial page setup for pops page

---
+++ build/.qt/QtDeployTargets.cmake
@@ -0,0 +1,2 @@
+set(__QT_DEPLOY_TARGET_water_FILE /Users/alexgeorge/Documents/User Interfaces CW/WaterQuality/build/water)
+set(__QT_DEPLOY_TARGET_water_TYPE EXECUTABLE)
---
+++ build/CMakeFiles/water_autogen.dir/cmake_clean.cmake
@@ -0,0 +1,10 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/water_autogen"
+  "water_autogen/mocs_compilation.cpp"
+  "water_autogen/timestamp"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang )
+  include(CMakeFiles/water_autogen.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
