7bd72303817451ef620f4d2af12e9da2728fe01f
Sure Wu <surewu@SuredeMacBook-Pro.local>
2024-12-05 04:05:03 +0000

    Update litter page styling to match POPs page

--- src/litterpage.cpp
+++ src/litterpage.cpp
@@ -2,23 +2,29 @@
 #include <QGridLayout>
 #include <QLabel>
 #include <set>
+#include "styles.h"
 
-LitterPage::LitterPage(QWidget* parent)
-    : BasePage("Litter Indicators", parent) {
+LitterPage::LitterPage(QWidget *parent)
+    : BasePage("Litter Indicators", parent)
+{
     loadData();
     setupUI();
+    setStyleSheet(Styles::combineStyleSheets({":/styles/basepage.qss",
+                                              ":/styles/litter.qss"}));
 }
 
-void LitterPage::loadData() {
+void LitterPage::loadData()
+{
     dataset.loadData("../dataset/Y-2024-M.csv");
 }
 
-void LitterPage::setupUI() {
+void LitterPage::setupUI()
+{
     mainLayout = new QVBoxLayout(contentArea);
     mainLayout->setContentsMargins(20, 20, 20, 20);
     mainLayout->setSpacing(20);
 
-    QLabel* titleLabel = new QLabel("Environmental Litter Analysis");
+    QLabel *titleLabel = new QLabel("Environmental Litter Analysis");
     titleLabel->setObjectName("pageTitle");
     mainLayout->addWidget(titleLabel);
 
@@ -28,115 +34,157 @@ void LitterPage::setupUI() {
     updateCharts();
 }
 
-void LitterPage::setupFilters() {
-    filtersLayout = new QHBoxLayout();
-    
-    QLabel* locationLabel = new QLabel("Location:");
+void LitterPage::setupFilters()
+{
+    // 创建Frame容器及其布局
+    controlsFrame = new QFrame();
+    controlsFrame->setObjectName("controlsFrame");
+    controlsLayout = new QHBoxLayout(controlsFrame);
+    controlsLayout->setSpacing(20);
+
+    // 创建标签和下拉框
+    QLabel *locationLabel = new QLabel("Location:");
+    locationLabel->setObjectName("filterLabel");
     locationFilter = new QComboBox();
 
-    QLabel* typeLabel = new QLabel("Water Type:");
+    QLabel *typeLabel = new QLabel("Water Type:");
+    typeLabel->setObjectName("filterLabel");
     waterTypeFilter = new QComboBox();
-    
+
+    // 填充下拉框数据
     std::set<QString> locations;
     std::set<QString> waterTypes;
-    
-    for(int i = 0; i < dataset.size(); i++) {
+
+    for (int i = 0; i < dataset.size(); i++)
+    {
         locations.insert(QString::fromStdString(dataset[i].getSamplingPointLabel()));
         waterTypes.insert(QString::fromStdString(dataset[i].getSampledMaterialType()));
     }
-    
+
     locationFilter->addItem("All Locations");
-    for(const auto& loc : locations) {
+    for (const auto &loc : locations)
+    {
         locationFilter->addItem(loc);
     }
-    
+
     waterTypeFilter->addItem("All Types");
-    for(const auto& type : waterTypes) {
+    for (const auto &type : waterTypes)
+    {
         waterTypeFilter->addItem(type);
     }
-    
-    filtersLayout->addWidget(locationLabel);
-    filtersLayout->addWidget(locationFilter);
-    filtersLayout->addSpacing(20);
-    filtersLayout->addWidget(typeLabel);
-    filtersLayout->addWidget(waterTypeFilter);
-    filtersLayout->addStretch();
-    
-    mainLayout->addLayout(filtersLayout);
-    
+
+    // 添加控件到controlsLayout
+    controlsLayout->addWidget(locationLabel);
+    controlsLayout->addWidget(locationFilter);
+    controlsLayout->addSpacing(20);
+    controlsLayout->addWidget(typeLabel);
+    controlsLayout->addWidget(waterTypeFilter);
+    controlsLayout->addStretch();
+
+    // 将Frame添加到主布局
+    mainLayout->addWidget(controlsFrame);
+
+    // 连接信号
     connect(locationFilter, &QComboBox::currentTextChanged,
             this, &LitterPage::onLocationFilterChanged);
     connect(waterTypeFilter, &QComboBox::currentTextChanged,
             this, &LitterPage::onWaterTypeFilterChanged);
 }
 
-void LitterPage::setupCharts() {
+void LitterPage::setupCharts()
+{
     chartsLayout = new QHBoxLayout();
+
+    // 创建Frame容器
+    QFrame* barChartFrame = new QFrame();
+    QFrame* pieChartFrame = new QFrame();
     
+    // 设置objectName以便应用qss
+    barChartFrame->setObjectName("chartFrame");
+    pieChartFrame->setObjectName("chartFrame");
+
+    QVBoxLayout* barLayout = new QVBoxLayout(barChartFrame);
+    QVBoxLayout* pieLayout = new QVBoxLayout(pieChartFrame);
+
+    // 创建图表
     barChart = new QChart();
     barChart->setTitle("Litter by Location");
     locationBarChart = new QChartView(barChart);
     locationBarChart->setRenderHint(QPainter::Antialiasing);
-    
+    locationBarChart->setDragMode(QGraphicsView::NoDrag);
+
     pieChart = new QChart();
     pieChart->setTitle("Distribution by Water Type");
     waterTypePieChart = new QChartView(pieChart);
     waterTypePieChart->setRenderHint(QPainter::Antialiasing);
-    
-    chartsLayout->addWidget(locationBarChart);
-    chartsLayout->addWidget(waterTypePieChart);
-    
+    waterTypePieChart->setDragMode(QGraphicsView::NoDrag);
+
+    // 添加到各自的布局
+    barLayout->addWidget(locationBarChart);
+    pieLayout->addWidget(waterTypePieChart);
+
+    // 添加Frame到主布局
+    chartsLayout->addWidget(barChartFrame);
+    chartsLayout->addWidget(pieChartFrame);
+
     mainLayout->addLayout(chartsLayout);
 }
 
-void LitterPage::updateCharts() {
+void LitterPage::updateCharts()
+{
     barChart->removeAllSeries();
     pieChart->removeAllSeries();
-    
-    QBarSeries* barSeries = new QBarSeries();
-    QPieSeries* pieSeries = new QPieSeries();
-    
+
+    QBarSeries *barSeries = new QBarSeries();
+    QPieSeries *pieSeries = new QPieSeries();
+
     std::map<QString, int> locationCounts;
     std::map<QString, int> waterTypeCounts;
-    
+
     QString selectedLocation = locationFilter->currentText();
     QString selectedWaterType = waterTypeFilter->currentText();
-    
-    for(int i = 0; i < dataset.size(); i++) {
+
+    for (int i = 0; i < dataset.size(); i++)
+    {
         QString location = QString::fromStdString(dataset[i].getSamplingPointLabel());
         QString waterType = QString::fromStdString(dataset[i].getSampledMaterialType());
-        
-        if((selectedLocation == "All Locations" || selectedLocation == location) &&
-           (selectedWaterType == "All Types" || selectedWaterType == waterType)) {
+
+        if ((selectedLocation == "All Locations" || selectedLocation == location) &&
+            (selectedWaterType == "All Types" || selectedWaterType == waterType))
+        {
             locationCounts[location]++;
             waterTypeCounts[waterType]++;
         }
     }
-    
-    QBarSet* barSet = new QBarSet("Sample Count");
+
+    QBarSet *barSet = new QBarSet("Sample Count");
     QStringList categories;
-    for(const auto& pair : locationCounts) {
+    for (const auto &pair : locationCounts)
+    {
         *barSet << pair.second;
         categories << pair.first;
     }
     barSeries->append(barSet);
-    
-    QBarCategoryAxis* axisX = new QBarCategoryAxis();
+
+    QBarCategoryAxis *axisX = new QBarCategoryAxis();
     axisX->append(categories);
     barChart->addSeries(barSeries);
     barChart->setAxisX(axisX, barSeries);
     barChart->createDefaultAxes();
-    
-    for(const auto& pair : waterTypeCounts) {
+
+    for (const auto &pair : waterTypeCounts)
+    {
         pieSeries->append(pair.first, pair.second);
     }
     pieChart->addSeries(pieSeries);
 }
 
-void LitterPage::onLocationFilterChanged(const QString&) {
+void LitterPage::onLocationFilterChanged(const QString &)
+{
     updateCharts();
 }
 
-void LitterPage::onWaterTypeFilterChanged(const QString&) {
+void LitterPage::onWaterTypeFilterChanged(const QString &)
+{
     updateCharts();
 }
--- src/litterpage.h
+++ src/litterpage.h
@@ -5,16 +5,17 @@
 #include "basepage.h"
 #include "dataset.h"
 
-class LitterPage : public BasePage {
+class LitterPage : public BasePage
+{
     Q_OBJECT
 
 public:
-    LitterPage(QWidget* parent = nullptr);
+    LitterPage(QWidget *parent = nullptr);
 
 private slots:
     void updateCharts();
-    void onLocationFilterChanged(const QString& location);
-    void onWaterTypeFilterChanged(const QString& type);
+    void onLocationFilterChanged(const QString &location);
+    void onWaterTypeFilterChanged(const QString &type);
 
 private:
     void setupUI() override;
@@ -22,17 +23,22 @@ private:
     void setupFilters();
     void loadData();
 
+    // 数据
     WaterQualityDataset dataset;
-    
-    QChartView* locationBarChart;
-    QChartView* waterTypePieChart;
-    QChart* barChart;
-    QChart* pieChart;
-    
-    QComboBox* locationFilter;
-    QComboBox* waterTypeFilter;
-    
-    QVBoxLayout* mainLayout;
-    QHBoxLayout* filtersLayout;
-    QHBoxLayout* chartsLayout;
+
+    // 控制区域组件
+    QFrame *controlsFrame;
+    QHBoxLayout *controlsLayout;
+    QComboBox *locationFilter;
+    QComboBox *waterTypeFilter;
+
+    // 图表组件
+    QChartView *locationBarChart;
+    QChartView *waterTypePieChart;
+    QChart *barChart;
+    QChart *pieChart;
+
+    // 布局
+    QVBoxLayout *mainLayout;
+    QHBoxLayout *chartsLayout;
 };
